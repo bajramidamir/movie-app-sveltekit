55f0d4b8b8699b7a55e3069101463f8668904367
Tempest20 <bajrami.damir@gmail.com>
2023-01-16 19:58:14 +0100

    swap tech stack

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script context="module">
     import "../style/main.css";
 
-
-    
-
-
+    export async function load ({ fetch }) {
+        const res = await fetch (
+            `https://api.themoviedb.org/3/movie/popular?api_key={import.meta.env.VITE_API_KEY}&language=en-US&page=1`
+        );
+        const data = await res.json();
+        if (res.ok) {
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
 </script>
